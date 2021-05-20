50e94a27358e0d822c4404f5807d2f19817a5249
Ryan Wolters <rywolters@gmail.com>
2021-05-19 23:39:18 -0500

    Added mode switching & added color shifting mode

--- color_clock.v
+++ color_clock.v
@@ -1,10 +1,11 @@
 // Author: Ryan Wolters
 
-module color_clock(clk_50, h_sync, v_sync, R, G, B, clk);
+module color_clock(clk_50, h_sync, v_sync, R, G, B, clk, sw);
 input clk_50;
+input reg[2:0] sw;
 output h_sync, v_sync, R, G, B, clk;
 
-// gen clk signal
+// generate 25MHz clock signal
 reg clk;
 always@(posedge clk_50)
 	clk = ~clk;
@@ -16,16 +17,36 @@ hvsync_gen sync_gen(clk, h_sync, v_sync, onscreen);
 reg[31:0] count;
 reg[2:0] color;
 wire msb_count = count[31];
-always@(posedge clk)
+always@(negedge v_sync)
 begin
 	count <= count + 1;
 	if(msb_count)
 		color = color + 1;
 end
 
-// color pattern
-assign R = onscreen;
-assign G = onscreen;
-assign B = onscreen; 
+// Mode switcher
+always@(negedge v_sync)
+	case(sw)
+		3'd0:
+			begin // generate white screen
+			assign R = onscreen;
+			assign G = onscreen;
+			assign B = onscreen;
+			end
+		3'd1:
+			begin // shifting color patterns
+			assign R = color[0] & onscreen;
+			assign G = color[1] & onscreen;
+			assign B = color[2] & onscreen;
+			end
+		default:
+			begin // generate white screen
+			assign R = onscreen;
+			assign G = onscreen;
+			assign B = onscreen;
+			end
+	endcase
+
+end
 
 endmodule
